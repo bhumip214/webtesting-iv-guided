ebb040860121925d60d9eafd8bb6731b433af28f
bhumip214 <bhumip214@gmail.com>
2019-03-07 11:05:52 -0800

    added production config

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,48 @@
 // Update with your config settings.
+const localPgConnection = {
+  host: "localhost",
+  database: "lambda",
+  user: "student",
+  passwrod: "hired!"
+};
+
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/hobbits.db3',
+      filename: "./data/hobbits.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/test.db3',
+      filename: "./data/test.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
+  production: {
+    client: "pg",
+    connection: prodDbConnection, // an object or a string
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
